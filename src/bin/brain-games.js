a9654b8dab9c239b952e2ec81b12f157bd427078
susorov.pavel <susorovpp@gmail.com>
2019-09-15 20:56:19 +0300

    do step 3

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,14 @@
-#!/usr/bin/node
-console.log('Welcome to the Brain Games!')
+#!/usr/bin/env node
+
+import {getUserName} from "..";
+
+console.log('Welcome to the Brain Games!');
+
+getUserName();
+
+
+
+
+
+
+
